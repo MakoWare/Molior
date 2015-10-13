1984401f59b5991db5983140c525388ce718f85c
yintothayang <yintothayang@gmail.com>
2015-10-12 23:52:22 -0700

    getting somewhere....

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash, make_response, send_file
 import json
 import subprocess
+import os
+
 app = Flask(__name__)
 
 repositoryName = ""
@@ -25,10 +27,34 @@ def parseResponse(response):
     return
 
 def run():
-    p = subprocess.Popen([command, argument1,...], cwd=working_directory)
-    p.wait()
+
+    ##First Change the Directory
+    f = open('config.txt', 'r')
+    config = json.loads(f.read())
+    dir = config['dir']
+    commands = config['commands']
+    print(dir)
+    retval = os.getcwd()
+    print ("Current working directory %s" % retval)
+    os.chdir(os.path.expanduser(dir))
+    retval = os.getcwd()
+    print ("Directory changed successfully %s" % retval)
+
+    print(commands)
+    for command in commands:
+         print(command)
+         subprocess_cmd(command)
+
+    #p = subprocess.Popen([command, argument1,...], cwd=working_directory)
+    #p.wait()
+
+def subprocess_cmd(command):
+    process = subprocess.Popen(command,stdout=subprocess.PIPE, shell=True)
+    proc_stdout = process.communicate()[0].strip()
+    print(proc_stdout)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=80)
-    app.run()
+    run()
+    # app.debug = True
+    # app.run(host='0.0.0.0', port=80)
+    # app.run()
